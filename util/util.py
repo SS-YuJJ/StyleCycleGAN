2c8be22186f7242cec0d28774c5bd44f61c47545
Jingji Yu <48582665+SS-YuJJ@users.noreply.github.com>
2022-08-17 03:01:01 +0800

    Update util.py

--- util/util.py
+++ util/util.py
@@ -8,6 +8,8 @@ import torch.nn.functional as F
 import torchvision
 
 
+
+
 def tensor2im(input_image, imtype=np.uint8):
     """"Converts a Tensor array into a numpy image array.
 
@@ -18,27 +20,22 @@ def tensor2im(input_image, imtype=np.uint8):
     input_image = torchvision.utils.make_grid(
         input_image[0], 
         0, 
-        normalize= True
+        # normalize= True
     )
     
     if len(input_image.size()) == 4:
         input_image = input_image[0]
 
 
-    if not isinstance(input_image, np.ndarray):
-        if isinstance(input_image, torch.Tensor):  # get the data from a variable
-            image_tensor = input_image.data
-        else:
-            return input_image
+    if isinstance(input_image, torch.Tensor):  # get the data from a variable
+        image_tensor = input_image.data
+    else:
+        return input_image
 
-        image_numpy = image_tensor.cpu().float().numpy()  # convert it into a numpy array
-        
-        if image_numpy.shape[0] == 1:  # grayscale to RGB
-            image_numpy = np.tile(image_numpy, (3, 1, 1))
+    image_numpy = image_tensor.cpu().float().numpy()  # convert it into a numpy array
 
-        image_numpy = (np.transpose(image_numpy, (1, 2, 0)))  * 255.0  # post-processing: tranpose and scaling
-    else:  # if it is a numpy array, do nothing
-        image_numpy = input_image
+    image_numpy = (np.transpose(image_numpy, (1, 2, 0))) * 255.0  # post-processing: tranpose and scaling
+
     return image_numpy.astype(imtype)
 
 
